4cbb3f33cce9ee458c1f30ad0836689772d65274
Mark Delk <jethrodaniel@gmail.com>
2025-04-08 19:56:09 -0500

    bundle gem grouping --exe --no-coc --no-mit --test=rspec --changelog --ci=github --linter=standard

---
+++ lib/grouping.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "grouping/version"
+
+module Grouping
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/grouping_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Grouping do
+  it "has a version number" do
+    expect(Grouping::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
