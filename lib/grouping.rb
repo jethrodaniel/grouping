920fca6e19462907613c2f799977704ea2c14d14
Mark Delk <jethrodaniel@gmail.com>
2025-04-08 22:37:13 -0500

    take one

--- lib/grouping.rb
+++ lib/grouping.rb
@@ -2,7 +2,78 @@
 
 require_relative "grouping/version"
 
+require "csv"
+
 module Grouping
-  class Error < StandardError; end
-  # Your code goes here...
+  Error = Class.new(StandardError)
+
+  module Sequences
+    class IntegerSequence
+      def initialize
+        @enum = (1..).to_enum
+      end
+
+      def next = @enum.next
+    end
+  end
+
+  module Strategies
+    class SameEmail
+      def call identifiers, sequence, row
+        field = row["Email1"]
+
+        if identifiers.key? field
+          identifiers[field]
+        else
+          identifiers[field] = sequence.next
+        end
+      end
+    end
+
+    class SamePhone
+      def call identifiers, sequence, row
+        field = row["Phone1"]
+
+        if identifiers.key? field
+          identifiers[field]
+        else
+          identifiers[field] = sequence.next
+        end
+      end
+    end
+
+    class SameEmailOrPhone
+      def call identifiers, sequence, row
+        fields = %w[Email1 Phone1]
+
+        id = if fields.any? { |field| identifiers.key?(row[field]) }
+          field = fields.find { |field| identifiers.key?(row[field]) }
+          identifiers[row[field]]
+        else
+          sequence.next
+        end
+
+        fields.each { |field| identifiers[row[field]] = id }
+
+        id
+      end
+    end
+  end
+
+  def self.group input_io:, output_io:, strategy:, sequence:
+    input_enum = CSV.new(input_io, headers: true).each
+    headers = input_enum.next.headers
+
+    output_io.puts CSV.generate_line(%w[user_id] + headers)
+
+    identifiers = {}
+
+    input_enum.each do |row|
+      id = strategy.call(identifiers, sequence, row)
+
+      output_io.puts CSV.generate_line([id] + row.fields)
+    end
+  rescue => e
+    raise Error, "#{e.class}: #{e.message}"
+  end
 end
--- spec/grouping_spec.rb
+++ spec/grouping_spec.rb
@@ -5,7 +5,70 @@ RSpec.describe Grouping do
     expect(Grouping::VERSION).not_to be_nil
   end
 
-  it "does something useful" do
-    expect(false).to be(true)
+  describe ".group" do
+    let(:input_io) do
+      StringIO.new(<<~CSV)
+        FirstName,LastName,Phone1,Phone2,Email1,Email2,Zip
+        John,Doe,(555) 123-4567,(555) 987-6543,johnd@home.com,,94105
+        Jane,Doe,(555) 123-4567,(555) 654-9873,janed@home.com,johnd@home.com,94105
+        Jack,Doe,(444) 123-4567,(555) 654-9873,jackd@home.com,,94109
+        John,Doe,(555) 123-4567,(555) 987-6543,jackd@home.com,,94105
+        Josh,Doe,(456) 789-0123,,joshd@home.com,jackd@home.com,94109
+        Jill,Doe,(654) 987-1234,,jill@home.com,,94129
+      CSV
+    end
+    let(:output_io) { StringIO.new }
+    let(:sequence) { described_class::Sequences::IntegerSequence.new }
+
+    context "when strategy is by Email1" do
+      let(:strategy) { Grouping::Strategies::SameEmail.new }
+
+      it "groups by email" do
+        described_class.group(input_io:, output_io:, strategy:, sequence:)
+
+        expect(output_io.tap(&:rewind).read).to eq(<<~CSV)
+          user_id,FirstName,LastName,Phone1,Phone2,Email1,Email2,Zip
+          1,Jane,Doe,(555) 123-4567,(555) 654-9873,janed@home.com,johnd@home.com,94105
+          2,Jack,Doe,(444) 123-4567,(555) 654-9873,jackd@home.com,,94109
+          2,John,Doe,(555) 123-4567,(555) 987-6543,jackd@home.com,,94105
+          3,Josh,Doe,(456) 789-0123,,joshd@home.com,jackd@home.com,94109
+          4,Jill,Doe,(654) 987-1234,,jill@home.com,,94129
+        CSV
+      end
+    end
+
+    context "when strategy is by Phone1" do
+      let(:strategy) { Grouping::Strategies::SamePhone.new }
+
+      it "groups by email" do
+        described_class.group(input_io:, output_io:, strategy:, sequence:)
+
+        expect(output_io.tap(&:rewind).read).to eq(<<~CSV)
+          user_id,FirstName,LastName,Phone1,Phone2,Email1,Email2,Zip
+          1,Jane,Doe,(555) 123-4567,(555) 654-9873,janed@home.com,johnd@home.com,94105
+          2,Jack,Doe,(444) 123-4567,(555) 654-9873,jackd@home.com,,94109
+          1,John,Doe,(555) 123-4567,(555) 987-6543,jackd@home.com,,94105
+          3,Josh,Doe,(456) 789-0123,,joshd@home.com,jackd@home.com,94109
+          4,Jill,Doe,(654) 987-1234,,jill@home.com,,94129
+        CSV
+      end
+    end
+
+    context "when strategy is by Email1 or Phone1" do
+      let(:strategy) { Grouping::Strategies::SameEmailOrPhone.new }
+
+      it "groups by email or phone" do
+        described_class.group(input_io:, output_io:, strategy:, sequence:)
+
+        expect(output_io.tap(&:rewind).read).to eq(<<~CSV)
+          user_id,FirstName,LastName,Phone1,Phone2,Email1,Email2,Zip
+          1,Jane,Doe,(555) 123-4567,(555) 654-9873,janed@home.com,johnd@home.com,94105
+          2,Jack,Doe,(444) 123-4567,(555) 654-9873,jackd@home.com,,94109
+          2,John,Doe,(555) 123-4567,(555) 987-6543,jackd@home.com,,94105
+          3,Josh,Doe,(456) 789-0123,,joshd@home.com,jackd@home.com,94109
+          4,Jill,Doe,(654) 987-1234,,jill@home.com,,94129
+        CSV
+      end
+    end
   end
 end
